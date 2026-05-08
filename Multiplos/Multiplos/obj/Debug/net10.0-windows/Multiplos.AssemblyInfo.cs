6c766e5ef468e230e6a76c71c6aa0dad1e913bed
Reverdan Almeida Sparinger <reverdan@gmail.com>
2026-05-08 19:25:21 -0300

    Inicio Multiplos

--- Multiplos/Multiplos/obj/Debug/net10.0-windows/Multiplos.AssemblyInfo.cs
+++ Multiplos/Multiplos/obj/Debug/net10.0-windows/Multiplos.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Temperatura")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Multiplos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e4bf69e5273ed6cf2cff26f98ddcefb19035c9")]
-[assembly: System.Reflection.AssemblyProductAttribute("Temperatura")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Temperatura")]
+[assembly: System.Reflection.AssemblyProductAttribute("Multiplos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Multiplos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
